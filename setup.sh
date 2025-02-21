92ae282a721491715ad41ee6e02b9cb7abfa6c40
Christian Johnson <christianjohnson@MacBook-Pro-von-Christian.local>
2025-02-21 16:52:52 +0100

    extend setup script with other dependencies

--- setup.sh
+++ setup.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-# todo: install packages
 
-# Clone the dotfiles repo
+# exit if not macOS, because dependencies below only work for mac
+if [[ "$(uname)" != "Darwin" ]]; then
+  echo "This script is intended for macOS only."
+  exit 1
+fi
+
+
+echo "installing dependencies ..."
+
+# nodejs, per the website
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+node -v
+nvm current
+npm -v
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# other dependencies
+brew install neovim ripgrep lazygit
+
+
+# clone repo with configurations
 git clone --bare https://github.com/christianj6/.dotfiles.git $HOME/.dotfiles
 
-# Create symlinks
+
+# create symlinks
 ln -s ~/.dotfiles/ghostty ~/.config/ghostty
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 
-echo "Dotfiles setup complete!"
+echo "setup complete."
