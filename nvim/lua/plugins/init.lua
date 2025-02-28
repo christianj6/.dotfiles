b9aaf31cd6d55197456122245be076f6877d1fe3
Christian Johnson <christianjohnson@MacBook-Pro-von-Christian.local>
2025-02-28 09:17:12 +0100

    add nvim surround

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -47,8 +47,18 @@ return {
     "sphamba/smear-cursor.nvim",
     lazy = true,
     opts = {},
-  }
+  },
 
+  {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
