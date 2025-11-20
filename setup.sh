b082142c93a21bc75cbefd8e95cd0e9f5e1d9b77
christianj6 <chr.a.johnson@gmail.com>
2025-11-20 08:12:05 +0100

    add maccy to config

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ elif [[ "$OS" == "macos" ]]; then
     if ! command -v brew &> /dev/null; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-    brew install neovim ripgrep lazygit derailed/k9s/k9s bear wget
+    brew install neovim ripgrep lazygit derailed/k9s/k9s bear wget maccy
 fi
 
 # Install k9s on Linux
